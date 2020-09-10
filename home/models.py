84df0c5711da1a22f27892b9c36d21ebfb6ae9db
Andrey <polopolaw@gmail.com>
2020-09-10 23:32:42 +0600

    Вынес редактирование слайдера на главной в админку, yandex аналитика

--- home/models.py
+++ home/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django import forms
 from datetime import datetime
 from modelcluster.fields import ParentalKey
-
-
 from wagtail.users.forms import UserEditForm, UserCreationForm
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
@@ -15,6 +13,10 @@ from blog.models import BlogPage
 from event.models import EventPage
 
 class HomePage(Page):
+    content_panels = Page.content_panels + [
+        InlinePanel('slider', label="Слайдер в шапке"),
+    ]
+
     def get_context(self, request):
         # Update context to include only published posts, ordered by reverse-chron
         context = super().get_context(request)
@@ -24,6 +26,33 @@ class HomePage(Page):
         context['events'] = events
         return context
 
+
+class SliderToHomePage(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='slider')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    title = models.CharField(max_length=90, help_text="Заголовок")
+    sub_title = models.CharField(max_length=90, help_text="Подзаголовок")
+    button_text_1 = models.CharField(max_length=90)
+    button_href_1 = models.URLField(blank=True)
+    button_text_2 = models.CharField(max_length=90, blank=True, null=True)
+    button_href_2 = models.URLField(blank=True)
+    
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('title'),
+        FieldPanel('sub_title'),
+        MultiFieldPanel([
+        FieldPanel('button_text_1'),
+        FieldPanel('button_href_1'),
+        ]),
+        MultiFieldPanel([
+        FieldPanel('button_text_2'),
+        FieldPanel('button_href_2'),
+        ]),
+        
+    ]
 
 class CustomHTMLPage(Page):
     
